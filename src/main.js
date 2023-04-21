e3affda92502898703beec1faffd20a0f9071bee
lzyjin <1247y.j@gmail.com>
2023-04-21 22:30:17 +0900

    vue3 프로젝트 생성

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+// 싱글 파일 컴포넌트에서 최상위 컴포넌트 앱을 가져옵니다.
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
